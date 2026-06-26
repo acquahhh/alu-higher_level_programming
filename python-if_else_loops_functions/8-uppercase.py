2891dec1df4a93697a6a83d60f536e0375892194
acquahhh <e.adomanu@alustudent.com>
2026-06-26 20:40:33 +0200

    Update 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    # TODO: print str in uppercase, then a newline
-    pass
-
-
-uppercase("hello")
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            print("{:c}".format(ord(c) - 32), end="")
+        else:
+            print("{:c}".format(ord(c)), end="")
+    print("")
