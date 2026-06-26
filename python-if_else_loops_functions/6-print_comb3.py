bea99b95a8cfa8162cdff86c1e208e68cd31d78f
acquahhh <e.adomanu@alustudent.com>
2026-06-26 20:39:57 +0200

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-# TODO: print all unique combinations of two different digits
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}".format(i, j), end=", ")
