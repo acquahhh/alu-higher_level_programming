056d88f34b9635ca5bd3189e0115b087417d4429
acquahhh <e.adomanu@alustudent.com>
2026-06-26 20:42:11 +0200

    Update 12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    # TODO: implement FizzBuzz from 1 to 100, space-separated
-    pass
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print("{:d}".format(i), end=" ")
