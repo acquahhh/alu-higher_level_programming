3d8f3972a6cbdabb9a335a6352e2e0edcee50ca2
acquahhh <e.adomanu@alustudent.com>
2026-06-26 20:40:15 +0200

    Update 7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    # TODO: return True if c is a lowercase letter
-    pass
-
-
-print(islower("a"))
-print(islower("H"))
+    return ord('a') <= ord(c) <= ord('z')
