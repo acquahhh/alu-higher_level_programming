b1023845b24f723a416b4d707eaca4f0403d4863
qwekua <adomanua@gmail.com>
2026-06-26 20:51:30 +0200

    task 9: corrected 8-uppercase.py in python-if_else_loops_functions

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for c in str:
         if ord('a') <= ord(c) <= ord('z'):
-            print("{:c}".format(ord(c) - 32), end="")
+            result += "{:c}".format(ord(c) - 32)
         else:
-            print("{:c}".format(ord(c)), end="")
+            result += "{:c}".format(ord(c))
+    print("{}".format(result), end="")
     print("")
